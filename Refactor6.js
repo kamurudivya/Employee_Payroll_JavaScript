3e1fe42187eef9056d5a0c47012d1fa9d9a64dfc
kamurudivya <93344763+kamurudivya@users.noreply.github.com>
2022-01-17 22:26:17 +0530

    Added : Storing daily wages along with array and calculated total wage

---
+++ Refactor6.js
@@ -0,0 +1,41 @@
+// UC3
+/* 
+*Refactor code And write function to get work hours
+*/
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+
+
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+//UC6
+/*
+* function for calculating daily wage
+*/
+function calculateDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    var empCheck = Math.floor(Math.random() * 10) % 3;
+    var empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calculateDailyWage(empHrs));
+}
+var empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days:" + totalWorkingDays + "," + " Total Hour(s:" + totalEmpHrs + "," + " Employee Wage: " + empWage + "," + " Daily Wages: " + "[" + empDailyWageArr + "]");
+console.log();
